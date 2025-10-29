f348be89974dcfe595f06315dd05d37025c8e5c1
ayim-en <ayimen.dev@gmail.com>
2025-10-29 14:22:24 -0700

    feat: fixing up upcoming prayer section, restarting carousel

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,4 @@
-import {
-  getPrayerDict,
-  PrayerDict,
-  Timings,
-} from "@/prayer-api/prayerTimesAPI";
+import { getPrayerDict, PrayerDict } from "@/prayer-api/prayerTimesAPI";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   ActivityIndicator,
@@ -119,15 +115,15 @@ export default function Index() {
   );
   const nextPrayer = useMemo(() => getNextPrayer(prayerDict), [prayerDict]);
 
-  const convertToPrayerRows = (isoDate: string): PrayerRow[] => {
-    const day = prayerDict[isoDate];
-    if (!day) return [];
-    return Prayers.map((prayerName) => ({
-      id: `${isoDate}-${prayerName}`,
-      prayer: prayerName,
-      time: day.timings[prayerName as keyof Timings],
-    }));
-  };
+  // const convertToPrayerRows = (isoDate: string): PrayerRow[] => {
+  //   const day = prayerDict[isoDate];
+  //   if (!day) return [];
+  //   return Prayers.map((prayerName) => ({
+  //     id: `${isoDate}-${prayerName}`,
+  //     prayer: prayerName,
+  //     time: day.timings[prayerName as keyof Timings],
+  //   }));
+  // };
 
   if (loading) return <ActivityIndicator className="mt-12" />;
   if (error) return <Text className="color-red-600 m-4">{error}</Text>;
@@ -138,9 +134,10 @@ export default function Index() {
         source={require("../../assets/images/prayer-pro-bg/prayer-pro-bg-fajr.png")}
         className="absolute top-0 left-0 w-full h-full"
       />
+      {/* //TODO: background will change depending on upcoming prayer*/}
 
       <View className="absolute left-0 right-0 justify-center items-center pt-24 mb-14">
-        <Text className="text-sm text-white">
+        <Text className=" font-bold text-sm text-white">
           {nextPrayer
             ? `Upcoming Prayer: ${nextPrayer.prayer}`
             : "Loading prayer times..."}
@@ -148,72 +145,15 @@ export default function Index() {
         <Text className="font-extrabold text-white text-[80px]">
           {nextPrayer ? nextPrayer.time : "--:--"}
         </Text>
-        <Text className="text-sm font-bold text-white">Seattle, WA</Text>
+        <Text className="text-sm font-bold text-white">Tokyo, Japan</Text>
       </View>
-      {/* //Todo: Replace With Current Location*/}
+      {/* //Todo: Replace With Current Location and Add compass icon*/}
 
       <View className="pt-64 items-center mt-32" style={{ marginBottom: 48 }}>
         <View
           className="relative"
           style={{ width: pageWidth, height: pageHeight }}
-        >
-          <FlatList
-            ref={(r) => (flatListRef.current = r)}
-            data={sortedDates}
-            horizontal
-            pagingEnabled
-            initialScrollIndex={todayIndex}
-            decelerationRate="fast"
-            showsHorizontalScrollIndicator={false}
-            keyExtractor={(item) => item}
-            snapToInterval={pageWidth}
-            getItemLayout={(_, index) => ({
-              length: pageWidth,
-              offset: pageWidth * index,
-              index,
-            })}
-            renderItem={({ item: isoDate }) => {
-              const day = prayerDict[isoDate];
-              // const prayerRows = convertToPrayerRows(isoDate);
-
-              return (
-                <View
-                  key={isoDate}
-                  style={{ width: pageWidth }}
-                  className="h-full"
-                >
-                  <View
-                    className="flex-row items-center justify-between px-8 py-4"
-                    style={{ backgroundColor: "#568FAF" }}
-                  >
-                    <Text className="text-lg font-bold text-white">
-                      {day.weekday} {day.gregorianDate}
-                    </Text>
-                    <Text className="text-lg font-bold text-white">
-                      {day.hijriDate} {day.designation.abbreviated}
-                    </Text>
-                  </View>
-
-                  <View
-                    style={{ backgroundColor: "#568FAF" }}
-                    className="w-full flex-1"
-                  >
-                    {Prayers.map((prayer) => (
-                      <View
-                        key={`row-${prayer}`}
-                        className="flex-1 flex-row items-center px-8"
-                      >
-                        <View className="flex-1 h-[1px] bg-white opacity-20" />
-                        <Text className="text-xl text-white mx-2">•</Text>
-                        <View className="flex-1 h-[1px] bg-white opacity-20" />
-                      </View>
-                    ))}
-                  </View>
-                </View>
-              );
-            }}
-          />
-        </View>
+        ></View>
       </View>
     </View>
   );
